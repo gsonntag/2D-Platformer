0fb5fc82d5520b1859f599a8ec52e2319cba97d9
Gavin <gsag@ucla.edu>
2024-09-28 13:47:15 -0700

    Update

--- Assets/Scenes/code/Cirkul.cs
+++ Assets/Scenes/code/Cirkul.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Circle : MonoBehaviour
+public class Cirkul : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello");
+        Debug.Log("test");
     }
 
     // Update is called once per frame
